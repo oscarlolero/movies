6f3a59fdebc723cd9f29725c54becb7118bea61b
Oscar Montes <oscmcojc1@live.com.mx>
2021-01-13 10:19:59 -0600

    Card swiper

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:movies/src/widgets/card_swiper_widget.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Movies in cinemas'),
+        backgroundColor: Colors.indigoAccent,
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+      ),
+      body: Container(
+        child: Column(children: [
+          _cardSwiper(),
+        ]),
+      ),
+    );
+  }
+
+  Widget _cardSwiper() {
+    return CardSwiper(movies: [
+      1,2,3,4,5
+    ]);
+  }
+}
